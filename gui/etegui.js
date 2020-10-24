7387136cba0543f9a1c29162e6235ad87c26a8b3
Jordi <jordi.burguet.castell@gmail.com>
2020-10-24 10:19:26 +0200

    Change style thru the DOM rather than from an update to the server
    Changes in colors, font type and size, can be made by changing the
    css, so it is faster than repainting all again.
    
    Also, prepare the js client to receive texts.

--- gui/etegui.js
+++ gui/etegui.js
@@ -14,8 +14,10 @@ const view = {
   update_on_drag: true,
   drag: {x0: 0, y0: 0, active: false},  // used when dragging the image
   line_color: "#000",
-  rect_color: "#00F",
+  rect_color: "#0A0",
+  text_color: "#00A",
   font_family: "sans-serif",
+  font_size: 10,
   minimap_show: true
 };
 const dgui = new dat.GUI();
@@ -27,11 +29,17 @@ dgui_ctl.add(view.tl, "y").name("top-left y").onChange(update);
 dgui_ctl.add(view, "zoom", 1, 100).onChange(update);
 dgui_ctl.add(view, "update_on_drag").name("continuous dragging");
 const dgui_style = dgui.addFolder("style");
-dgui_style.addColor(view, "line_color").name("line color").onChange(update);
-dgui_style.addColor(view, "rect_color").name("rectangle color").onChange(update);
+dgui_style.addColor(view, "line_color").name("line color").onChange(() =>
+  document.styleSheets[0].cssRules[1].style.stroke = view.line_color);
+dgui_style.addColor(view, "rect_color").name("rectangle color").onChange(() =>
+  document.styleSheets[0].cssRules[2].style.stroke = view.rect_color);
+dgui_style.addColor(view, "text_color").name("text color").onChange(() =>
+  document.styleSheets[0].cssRules[3].style.fill = view.text_color);
 dgui_style.add(view, "font_family",
   ["sans-serif", "serif", "cursive", "monospace"]).name("font").onChange(() =>
-    div_tree.style.fontFamily = view.font_family);
+    document.styleSheets[0].cssRules[3].style.fontFamily = view.font_family);
+dgui_style.add(view, "font_size", 1, 20).onChange(() =>
+  document.styleSheets[0].cssRules[3].style.fontSize = `${view.font_size}px`);
 const dgui_minimap = dgui.addFolder("minimap");
 dgui_minimap.add(view, "minimap_show").name("active").onChange(() => {
     const display = view.minimap_show ? "block" : "none";
@@ -135,20 +143,27 @@ function draw(items, x0_, y0_, width_, height_) {
 
   let svg = `<svg width="${width}" height="${height}">`;
   items.forEach(d => {
-    switch (d[0]) {
-      case 'r':
-        const x = d[1] - x0, y = d[2] - y0, w = d[3], h = d[4];
-        svg += `<rect x="${x}" y="${y}" width="${w}" height="${h}"
-                      fill="none" stroke="${view.rect_color}"/>`;
-        break;
-      case 'l':
-        const x1 = d[1] - x0, y1 = d[2] - y0,
-              x2 = d[3] - x0, y2 = d[4] - y0;
-        svg += `<line x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}"
-                      stroke="${view.line_color}"/>`;
-        break;
-      default:
-        console.log(`Got unknown item: ${d}`);
+    if (d[0] === 'r') {       // rectangle
+      const x = d[1] - x0, y = d[2] - y0, w = d[3], h = d[4];
+      svg += `<rect class="rect" x="${x}" y="${y}" width="${w}" height="${h}"
+                    fill="none" stroke="${view.rect_color}"/>`;
+    }
+    else if (d[0] === 'l') {  // line
+      const x1 = d[1] - x0, y1 = d[2] - y0,
+            x2 = d[3] - x0, y2 = d[4] - y0;
+      svg += `<line class="line" x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}"
+                    stroke="${view.line_color}"/>`;
+    }
+    else if (d[0] === 't') {  // text
+      const x = d[1] - x0, y = d[2] - y0, w = d[3], h = d[4];
+      svg += `<text class="text" x="${x}" y="${y}"
+                    color="${view.text_color}">${d[5]}</text>`;
+      // TODO: check if we want to use something like:
+      //    textLength="${w}"
+      // TODO: compute the length of the text and adjust so it fits in the rect
+    }
+    else {
+      console.log(`Got unknown item: ${d[0]}`);
     }
   });
   svg += '</svg>';
