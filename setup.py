2a88ad1a988ca3038d550d85ac44d1d07de83dac
Jordi <jordi.burguet.castell@gmail.com>
2020-11-20 12:03:02 +0100

    Add the sql and data files to the distribution

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+from glob import glob
 
-setup(name='ete',
-      version='4.0',
-      description = 'Visualization of huge trees with smart zooming',
-      author='Jaime Huerta-Cepas, Jordi Burguet-Castell ',
-      author_email='jhcepas@gmail.com, jordi.burguet.castell@gmail.com',
-      license = 'GPLv3',
-      url='http://etetoolkit.org',
-      packages=['ete'],
-      package_data={'ete': ['static/*']})
+setup(
+    name='ete',
+    version='4.0',
+    description='Visualization of huge trees with smart zooming',
+    author='Jaime Huerta-Cepas, Jordi Burguet-Castell ',
+    author_email='jhcepas@gmail.com, jordi.burguet.castell@gmail.com',
+    license='GPLv3',
+    url='http://etetoolkit.org',
+    packages=['ete'],
+    data_files=[
+        ('server', glob('ete/static/gui.*')),
+        ('server/external', glob('ete/static/external/*')),
+        ('sql', glob('tests/*.sql')),
+        ('example_tree_data', glob('tests/example_tree_data/*'))])
