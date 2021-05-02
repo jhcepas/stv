0e86b775341fb538fc225266bfe32807eab9b961
Jordi <jordi.burguet.castell@gmail.com>
2021-05-02 20:23:52 +0200

    cosmetics - Start with the childrenline seen as dotted.

--- scripts/static/js/gui.js
+++ scripts/static/js/gui.js
@@ -99,7 +99,7 @@ const view = {
         children: {
             color: "#000",
             width: 1,
-            pattern: "solid",
+            pattern: "dotted",
         },
     },
     array: {padding: 0.0},
